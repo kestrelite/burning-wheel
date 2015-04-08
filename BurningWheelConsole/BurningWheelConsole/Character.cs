cf4aba5fadc7d3c5e1c5cef7f592abd208c2c097
noahsutsmo@gmail.com <noahsutsmo@gmail.com>
2015-04-08 03:07:30 -0700

    Added beliefs and instincts to Character

--- BurningWheelConsole/BurningWheelConsole/Character.cs
+++ BurningWheelConsole/BurningWheelConsole/Character.cs
@@ -8,7 +8,25 @@ namespace BurningWheelConsole
 {
     public class Character
     {
+        public string Name { set; get; }
+        public string Concept { set; get; }
         public string Race { set; get; }
+        public int Age { set; get; }
+
+        private List<String> _BeliefsList = new List<String>();
+        public List<String> BeliefsList
+        {
+            private set { }
+            get { return _BeliefsList; }
+        }
+
+        private List<String> _InstinctsList = new List<String>();
+        public List<String> InstinctsList
+        {
+            private set { }
+            get { return _InstinctsList; }
+        }
+
         private List<Lifepath> _LifepathList = new List<Lifepath>();
         public List<Lifepath> LifepathList 
         {
@@ -31,6 +49,26 @@ namespace BurningWheelConsole
                 return true;
             }
             return false;
+        }
+
+        public void AddBelief(string s)
+        {
+            _BeliefsList.Add(s);
+        }
+
+        public void DropBelief(int ind)
+        {
+            _BeliefsList.RemoveAt(ind);
+        }
+
+        public void AddInstinct(string s)
+        {
+            _InstinctsList.Add(s);
+        }
+
+        public void DropInstinct(int ind)
+        {
+            _InstinctsList.RemoveAt(ind);
         }
     }
 }
